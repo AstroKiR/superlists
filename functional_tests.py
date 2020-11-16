0d103cba51726ce2b189a6647b758f1661bfef6d
AstroKiR <eu1kp@yahoo.com>
2020-11-17 01:35:42 +0300

    New steps in FT, [GET, POST] home_page, helper method to check for row in list table

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -12,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # A new user visits the start page
@@ -25,26 +34,27 @@ class NewVisitorTest(unittest.TestCase):
         # He is invited to enter a to-do list straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
         # He types "Buy new tires"
         inputbox.send_keys('Buy new tires')
-
         # When he hits enter the page updates and he sees the '1: Buy new tires' as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy new tires')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy new tires' for row in rows), "New to-do item did not appear in table")
-
-        # There is still a text box inviting his to add another item. He enters "Change the tires"
-
-        self.fail('Finish the test!')
-
-
+        # There is still a text box inviting his to add another item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        # He enters "Change the tires"
+        inputbox.send_keys('Change the tires')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # The page updates again and he sees two rows in to-do list
         # 1: Buy new tires
         # 2: Change the tires
+        self.check_for_row_in_list_table('1: Buy new tires')
+        self.check_for_row_in_list_table('2: Change the tires')
+
+        self.fail('Finish the test!')
 
         # New use wonders whether the site will remember his list
         # Then he sees that the site has generated a unique URL for him -- there is some explanatory text to that effect
